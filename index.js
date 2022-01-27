f738f843d5bdf03a4e0f1af374e0691e7e9f0873
Nico Kaiser <nico@kaiser.me>
2022-01-27 17:28:11 +0100

    fix: replace eslint-config-koa with prettier

--- index.js
+++ index.js
@@ -27,22 +27,29 @@ module.exports = function encryptedSession(opts, app) {
   let secretKey;
   if (opts.secret) {
     if (Buffer.byteLength(opts.secret) < 32) {
-      throw new Error('Invalid key length: opts.secret must be at least 32 bytes');
+      throw new Error(
+        'Invalid key length: opts.secret must be at least 32 bytes'
+      );
     }
 
     secretKey = Buffer.allocUnsafe(sodium.crypto_secretbox_KEYBYTES);
     let salt = Buffer.from('r7bFNKlRQnpAjvuLawnvRQ==', 'base64');
-    if (opts.salt) salt = (Buffer.isBuffer(opts.salt)) ? opts.salt : Buffer.from(opts.salt, 'ascii');
+    if (opts.salt)
+      salt = Buffer.isBuffer(opts.salt)
+        ? opts.salt
+        : Buffer.from(opts.salt, 'ascii');
     if (Buffer.byteLength(salt) !== sodium.crypto_pwhash_SALTBYTES) {
       throw new Error('salt must be length ' + sodium.crypto_pwhash_SALTBYTES);
     }
 
-    sodium.crypto_pwhash(secretKey,
+    sodium.crypto_pwhash(
+      secretKey,
       Buffer.from(opts.secret),
       salt,
       sodium.crypto_pwhash_OPSLIMIT_MODERATE,
       sodium.crypto_pwhash_MEMLIMIT_MODERATE,
-      sodium.crypto_pwhash_ALG_DEFAULT);
+      sodium.crypto_pwhash_ALG_DEFAULT
+    );
   }
 
   if (opts.secretKey) {
@@ -53,7 +60,10 @@ module.exports = function encryptedSession(opts, app) {
       throw new Error('secretKey must be a string or a Buffer');
     }
 
-    if (secretKey.length < sodium.crypto_secretbox_KEYBYTES) throw new Error(`secretKey must be at least ${sodium.crypto_secretbox_KEYBYTES} bytes`);
+    if (secretKey.length < sodium.crypto_secretbox_KEYBYTES)
+      throw new Error(
+        `secretKey must be at least ${sodium.crypto_secretbox_KEYBYTES} bytes`
+      );
   }
 
   if (!secretKey) throw new Error('secretKey or secret must specified');
@@ -61,7 +71,7 @@ module.exports = function encryptedSession(opts, app) {
   // disable cookie signing
   opts.signed = false;
 
-  opts.decode = cookie => {
+  opts.decode = (cookie) => {
     const split = decodeURIComponent(cookie).split(';');
     const cyphertextB64 = split[0];
     const nonceB64 = split[1];
@@ -70,23 +80,31 @@ module.exports = function encryptedSession(opts, app) {
 
     const cipher = Buffer.from(cyphertextB64, 'base64');
     const nonce = Buffer.from(nonceB64, 'base64');
-    if (cipher.length < sodium.crypto_secretbox_MACBYTES) throw new SyntaxError('not long enough');
+    if (cipher.length < sodium.crypto_secretbox_MACBYTES)
+      throw new SyntaxError('not long enough');
 
-    const msg = Buffer.allocUnsafe(cipher.length - sodium.crypto_secretbox_MACBYTES);
-    if (!sodium.crypto_secretbox_open_easy(msg, cipher, nonce, secretKey)) throw new SyntaxError('unable to decrypt');
+    const msg = Buffer.allocUnsafe(
+      cipher.length - sodium.crypto_secretbox_MACBYTES
+    );
+    if (!sodium.crypto_secretbox_open_easy(msg, cipher, nonce, secretKey))
+      throw new SyntaxError('unable to decrypt');
 
     return JSON.parse(msg);
   };
 
-  opts.encode = session => {
+  opts.encode = (session) => {
     const nonce = Buffer.allocUnsafe(sodium.crypto_secretbox_NONCEBYTES);
     sodium.randombytes_buf(nonce);
     const msg = Buffer.from(JSON.stringify(session));
 
-    const cipher = Buffer.allocUnsafe(msg.length + sodium.crypto_secretbox_MACBYTES);
+    const cipher = Buffer.allocUnsafe(
+      msg.length + sodium.crypto_secretbox_MACBYTES
+    );
     sodium.crypto_secretbox_easy(cipher, msg, nonce, secretKey);
 
-    return encodeURIComponent(cipher.toString('base64') + ';' + nonce.toString('base64'));
+    return encodeURIComponent(
+      cipher.toString('base64') + ';' + nonce.toString('base64')
+    );
   };
 
   return session(opts, app);
--- test/encrypted-session.test.js
+++ test/encrypted-session.test.js
@@ -8,7 +8,7 @@ describe('Koa encrypted session', () => {
   describe('when opts not set', () => {
     it('should throw', () => {
       const app = new Koa();
-      (function() {
+      (function () {
         app.use(encryptedSession());
       }.should.throw('opts required: `encryptedSession(opts, app)`'));
     });
@@ -17,7 +17,7 @@ describe('Koa encrypted session', () => {
   describe('when app not set', () => {
     it('should throw', () => {
       const app = new Koa();
-      (function() {
+      (function () {
         app.use(encryptedSession({}));
       }.should.throw('app instance required: `encryptedSession(opts, app)`'));
     });
@@ -26,52 +26,60 @@ describe('Koa encrypted session', () => {
   describe('when app.secret is not set', () => {
     it('should throw', () => {
       const app = new Koa();
-      (function() {
+      (function () {
         app.use(encryptedSession({}, app));
       }.should.throw('secretKey or secret must specified'));
     });
   });
 
   describe('when opts.secret is set', () => {
-    it('should work', done => {
+    it('should work', (done) => {
       const app = new Koa();
 
-      app.use(encryptedSession({ secret: 'insert 32 random characters here' }, app));
+      app.use(
+        encryptedSession({ secret: 'insert 32 random characters here' }, app)
+      );
 
-      app.use(async ctx => {
+      app.use(async (ctx) => {
         ctx.session.message = 'hi';
         ctx.body = ctx.session;
       });
 
-      request(app.listen())
-        .get('/')
-        .expect(200, done);
+      request(app.listen()).get('/').expect(200, done);
     });
   });
 
   describe('when opts.secretKey is set', () => {
-    it('should work', done => {
+    it('should work', (done) => {
       const app = new Koa();
 
-      app.use(encryptedSession({ secretKey: '+qLPL3nkbs5tXqlisiisUBc3qqsowiOXcqtQfY8UAK0=' }, app));
+      app.use(
+        encryptedSession(
+          { secretKey: '+qLPL3nkbs5tXqlisiisUBc3qqsowiOXcqtQfY8UAK0=' },
+          app
+        )
+      );
 
-      app.use(async ctx => {
+      app.use(async (ctx) => {
         ctx.session.message = 'hi';
         ctx.body = ctx.session;
       });
 
-      request(app.listen())
-        .get('/')
-        .expect(200, done);
+      request(app.listen()).get('/').expect(200, done);
     });
   });
 
   describe('when the session contains a ;', () => {
-    it('should still work', done => {
+    it('should still work', (done) => {
       const app = new Koa();
-      app.use(encryptedSession({ secretKey: '+qLPL3nkbs5tXqlisiisUBc3qqsowiOXcqtQfY8UAK0=' }, app));
+      app.use(
+        encryptedSession(
+          { secretKey: '+qLPL3nkbs5tXqlisiisUBc3qqsowiOXcqtQfY8UAK0=' },
+          app
+        )
+      );
 
-      app.use(async ctx => {
+      app.use(async (ctx) => {
         if (ctx.method === 'POST') {
           ctx.session.string = ';';
           ctx.status = 204;
